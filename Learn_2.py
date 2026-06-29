ee44170b3f76745dce877a5780787af50308e873
kais2006183 <ttank6963@gmail.com>
2026-06-29 23:14:02 +0300

    Update Learn_2.py

--- Learn_2.py
+++ Learn_2.py
@@ -13,16 +13,13 @@
 # ==================================================================================================================
 
 # def calculate_bill(discount, *prices):
-#     # 1. نجمع كل الأسعار الممررة أولاً لتصبح قيمة واحدة (200 + 300 + 400 = 900)
 #     total_price = sum(prices)
     
-#     # 2. نطبق المعادلة الذكية التي تعلمتها على المجموع الكلي
 #     final_price = total_price - (total_price * discount / 100)
     
-#     # 3. نرجع النص باستخدام return بدلاً من print لمنع ظهور None
+
 #     return f"Total Price After Discount is: {final_price}"
 
-# # الآن عند طباعة استدعاء الدالة ستظهر النتيجة الإجمالية الموحدة مباشرة
 # print(calculate_bill(10, 200, 300, 400))
 # ==================================================================================================================
 
@@ -38,40 +35,35 @@
 # def process_user(name: str) -> str:
 #     global user_status
     
-#     # 1. ننظف الاسم أولاً في بداية الدالة
 #     clean_name = name.strip().capitalize()
     
-#     # 2. نفحص الاسم المنظف
 #     if clean_name == "Kais":
 #         user_status = "admin" # التغيير يحدث فقط لو كان Kais
 #         return f"User {clean_name} is processed."
     
-#     # 3. خروج بديل في حال كان المستخدم شخصاً آخر (حتى لا تظهر None)
 #     return f"User {clean_name} is a regular guest."
 
 # print(user_status)
-# print(process_user("   kais   ")) # ستعمل الآن بكفاءة وبدون فراغات
+# print(process_user("   kais   ")) 
 # print(user_status)
 # ==================================================================================================================
 # shortcut_double = lambda number :  number * 2
 # print(shortcut_double(15))
 # ==================================================================================================================
 
-# # القائمة التي نريد فلترتها
+
 # numbers_list = [12, 5, 20, 8, 15, 30, 7]
 
-# # الدالة التقليدية تستقبل القائمة، وتستقبل دالة الفحص (condition)
+
 # def filter_numbers(lst, condition):
 #     result = []
 #     for num in lst:
-#         # هنا الـ condition هي دالة الـ lambda التي سنمررها بالأسفل
 #         if condition(num) == True: 
 #             result.append(num)
 #     return result
 
-# # جرب الآن طباعة الاستدعاءات التالية وشاهد السحر في الـ Terminal:
-# print(filter_numbers(numbers_list, lambda x: x > 10))  # ستطبع الأرقام الأكبر من 10
-# print(filter_numbers(numbers_list, lambda x: x % 2 == 0))  # ستطبع الأرقام الزوجية
+# print(filter_numbers(numbers_list, lambda x: x > 10))
+# print(filter_numbers(numbers_list, lambda x: x % 2 == 0))  
 # ==================================================================================================================
 # try : # Try The Code 
 
